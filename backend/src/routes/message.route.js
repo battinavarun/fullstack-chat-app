1b5a14082005ea9408f72b83623111a324255165
Varun 31003 <battinavarun@gmail.com>
2025-07-13 22:17:37 +0530

    changed message route js file

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,9 +4,8 @@ import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/mes
 
 const router = express.Router();
 
-
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages)
+router.get("/messages/:id", protectRoute, getMessages) // Modified line
 
 router.post("/send/:id", protectRoute, sendMessage)
 
